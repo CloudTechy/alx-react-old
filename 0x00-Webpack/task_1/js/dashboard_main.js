3b959919623184e229c925e570fdeda0f9564ca7
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-08-23 16:02:38 +0100

    fix

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+let counter = 0;
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
@@ -8,7 +10,7 @@ $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter () {
-  const count = $('#count').html() || 0;
+  counter++;
   $('#count').html(parseInt(count) + 1 + ' clicks on the button');
 }
 
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  entry:  './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: __dirname + '/public',
     filename: 'bundle.js'
   }
 };
