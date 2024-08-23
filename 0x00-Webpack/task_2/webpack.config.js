ef1331367bb89a782550298885c315d1b197103f
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-08-23 17:33:07 +0100

    2. Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,64 @@
+const { watch, watchFile } = require("fs");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  performance: {
+    maxEntrypointSize: 1000000,
+    maxAssetSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        include: [path.resolve(__dirname, "css")],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext][query]", // Place images in public/assets/
+        },
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // Skip optimization in development
+              disable: true, // Disable the loader in development
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        include: [path.resolve(__dirname, "images")],
+      },
+    ],
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    ignored: /node_modules/
+  },
+  
+};
